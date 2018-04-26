91d20402a4eaca2219d49505c09b626acf4599b2
Caleb Boylan <calebboylan@gmail.com>
2018-04-25 22:42:13 -0700

    Add simple shell script to look for unimplemented ops
    This is mainly useful for devving purposes. Run the script and find
    opcodes that are not implemented from 0x00 - 0xFF, doesnt check
    0xCB00 - 0xCBFF ops

---
+++ check_unimplemented_ops.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in `seq 0 9` A B C D E F ; do
+    for j in `seq 0 9` A B C D E F ; do
+        res="$(grep "0x${i}${j} =>" src/gameboy/mod.rs)"
+        if [ -z "$res" ] ; then
+            echo "0x${i}${j}"
+        fi
+    done
+done
+
