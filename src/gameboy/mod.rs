3659ececc7dba9a4765830d4e58b3782ec52c171
Caleb Boylan <calebboylan@gmail.com>
2018-04-28 22:58:48 -0700

    Slow down execution to GB hardware speed

--- src/gameboy/mod.rs
+++ src/gameboy/mod.rs
@@ -1,10 +1,13 @@
 use std::time::{Duration, Instant};
+use std::thread;
 
 mod cpu;
 
 mod mmu;
 
 pub fn run_game(file_name: &str) {
+    let clock_speed: u64 = 4_194_000; // Hz
+    let nanos_per_cycle = Duration::from_nanos((1_000_000_000/(clock_speed/4)));
     let mut mem = mmu::Mmu::new();
     let mut cpu = cpu::Cpu::new();
     mem.load_rom(file_name);
@@ -12,8 +15,14 @@ pub fn run_game(file_name: &str) {
         let start = Instant::now();
         cpu.exec_next(&mut mem);
 
+        let elapsed = start.elapsed();
         if cfg!(debug_assertions = true) {
-            println!("elapsed nanos: {}", start.elapsed().subsec_nanos());
+            println!("elapsed nanos: {}", elapsed.subsec_nanos());
         }
+
+        if elapsed < nanos_per_cycle {
+            thread::sleep(nanos_per_cycle - elapsed);
+        }
+
     }
 }
