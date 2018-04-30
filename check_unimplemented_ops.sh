d6bc9f709ad0795d2c50b57249dea17d02ef5c0d
Caleb Boylan <calebboylan@gmail.com>
2018-04-29 20:22:06 -0700

    fix check_unimplemented_ops.sh for new file org

--- check_unimplemented_ops.sh
+++ check_unimplemented_ops.sh
@@ -2,7 +2,7 @@
 
 for i in `seq 0 9` A B C D E F ; do
     for j in `seq 0 9` A B C D E F ; do
-        res="$(grep "0x${i}${j} =>" src/gameboy/mod.rs)"
+        res="$(grep "0x${i}${j} =>" src/gameboy/cpu/mod.rs)"
         if [ -z "$res" ] ; then
             echo "0x${i}${j}"
         fi
