e9684dc1d7e440c910b6c3f1b79c7662635257b7
Caleb Boylan <calebboylan@gmail.com>
2018-07-23 15:04:03 -0700

    Fix the daa instruction

--- src/corroboy/cpu/ops/misc.rs
+++ src/corroboy/cpu/ops/misc.rs
@@ -5,40 +5,30 @@
 
 pub fn daa(a: &mut u8, flags: &mut u8) {
     let mut corr = 0;
+    if *flags & 0b00100000 != 0 {
+        corr |= 0x06;
+    }
+    if *flags & 0b00010000 != 0 {
+        corr |= 0x60;
+    }
     if *flags & 0b01000000 == 0 {
-        if *flags & 0b00100000 != 0 {
-            corr |= 0x06;
-        } else if *a & 0b00001111 > 9 {
+        if *a & 0b00001111 > 9 {
             corr |= 0x06;
         }
-        if *flags & 0b00010000 != 0 {
+        if *a > 0x99 {
             corr |= 0x60;
             *flags |= 0b00010000;
-        } else if (*a & 0b11110000) >> 4 > 9 {
-            corr |= 0x60;
-            *flags |= 0b00010000;
-        } else {
-            *flags &= 0b11101111;
-        }
-        *a += corr;
-    } else {
-        if *flags & 0b00010000 != 0 {
-            corr |= 0x60;
-            *flags |= 0b00010000;
-        } else {
-            *flags &= 0b11101111;
         }
 
-        if *flags & 0b00100000 != 0 {
-            corr |= 0x06;
-        }
+        *a += corr;
+    } else {
         *a -= corr;
     }
-    *flags &= 0b11011111;
+
+    *flags &= 0b01011111;
+
     if *a == 0 {
         *flags |= 0b10000000;
-    } else {
-        *flags &= 0b01111111;
     }
 }
 
